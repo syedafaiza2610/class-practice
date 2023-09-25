53dd23d7933b00962fb3756232764b12f418b414
syeda Faiza <syedafaiza2610@gmail.com>
2023-09-24 20:00:27 -0700

    class practice

---
+++ script.js
@@ -0,0 +1,301 @@
+// console.log("This is my message");
+// var num1 = 10;
+// var name = "SMIT";
+// console.log(name , "is my institute name" , ".");
+// console.log(name,num1);
+// // alert
+// var myName = "";
+// console.log("myName:" , myName);
+// var num1 = 10 + 2; //12
+// console.log(num1 + 2);
+// num1 = num1 + 3;
+// console.log(num1);
+
+// var myAge = 22;
+//  var num1 = 22 + 2;
+//  console.log(myAge);
+//  var age = "My age will be";
+//  var age1 = "after 2 years";
+//  console.log(myAge,age,num1,age1);
+
+//  var num2 = 10;
+//  console.log(10-2);
+//  console.log(10-"2");  //concadination
+// // ++ operator increase 1 karta hai 
+// // back tick k andar dollar ka sign or {} me dal dengen
+
+// console.log("my first line my second line");
+// console.log("num2 =" + num2 + "num3 =" + num3);
+// console.log('num2 ="${num2} num3 = ${num3}');
+// var_name = "";
+// var myClassNum = "";
+// var MK = 10;
+// var AbbbAJBHHJ = '';
+// var name ="My Name is Faiza";
+// var mybatch = "This is my JavaScript Class";
+// console.log(name , mybatch);
+// // alert(name+mybatch)
+// document.write(name + mybatch)
+// var num = 100;
+// console.log(num + 2);
+// console.log(num - 2);
+// console.log(num * 2);
+// console.log(num / 2);
+// console.log(num % 2);
+// console.log(num + '10' + 2);
+// console.log(num - 'ten' + 2);
+// console.log(5-(3+4));
+// console.log(10 - 2);
+// console.log('ten'- 2);
+// var para = "I got ";
+// var para2 = "Questions Correct out of 40 and I got 76%";
+// var total = 40;
+// var per = 77;
+// percentage = 100;
+// console.log(num2 / 2.5);
+// console.log(para + " " + "" +num2 / 2.5 + para2);
+
+// 
+// document.write(per / percentage);
+// document.write(per / percentage * total);
+// var percentage = prompt("Enter Your Percentage", 90);
+// var total = prompt("Enter Your Total Number Of Question" , 40);
+// var answer = percentage /100 * total;
+// console.log(answer);
+// console.log("I got", percentage,"%","and have done",answer,"correct");
+// var a;
+// a = 5;
+// var num = prompt("Enter your Number" , 10);
+// console.log("my num =", num + 2);
+// var age =+prompt("Enter your age");
+// var year = 2023;
+// var _age = 50 - age;
+// var _year = 2023 + _age;
+// console.log("you will be 50 years old in year" , _year);
+
+// if else
+
+// var num = 5;
+// if(num == 10){
+//     console.log("I am in if block");
+// } else{
+//     console.log("I am in else block");
+// }
+
+// var evenodd = +prompt();
+// if(evenodd % 2 == 0){
+//     document.write("You entered even number")
+// }
+// else{
+//     document.write("You entered odd number")
+// } 
+// var a = '10';
+// a == 10 //compare value //true
+// a === 10 //strict compare // false
+// a != 10 false
+// == compare
+// = assign
+// === compare
+// var attd = prompt("Enter your percentage");
+// if(attd >= 90){
+//     document.write("You are allowed in the class")
+// }else{
+//     document.write("You are not allowed in the class")
+// }
+//  var city = 'Karachi';
+//  prompt("Enter your city");
+
+//  if(city === 'Karachi'){
+//     document.write("Yes There is an Airport")
+//  }
+
+// ifelse (city === 'Faisalabad'){
+//     document.write("Yes Airport is under construction")
+//  }
+//  else{
+//     document.write("No There is no Airport")
+// }
+// var a = prompt("Enter Your city");
+//  var x = "Karachi";
+//  var y = "Islamabad";
+//  if(x <= 10 || y <= 5){
+//    document.write("Succesfull Login")
+//  }
+//  else if(y == 5){
+//    document.write("Login")
+//  }
+//  else{
+//    document.write("Failed")
+//  }
+//  if(a == x){
+//    document.write("Not Capital");
+//  }
+//  else if(a == y){
+//    document.write("It is a capital city")
+//  }
+//  else{
+//    document.write("Not a City")
+//  }wr
+// var city= "Islamabad"
+// var a = prompt("Enter your city")
+// if(city.toLocaleLowerCase !== "Karachi"){
+//    if (city.toLocaleLowerCase === "Islamabad"){
+//       document.write("capital")
+//    }
+// }
+// else{
+//    document.write("not a capital")}
+// var flowers = ["rose","jasmine","tulip"];
+// alert(flowers[0]);
+// alert(flowers[1]);
+// alert(flowers[2]);
+//  var userInput = +prompt("Enter your number");
+//  var userInput2 = +prompt("Enter your number");
+//  for(var i=1; i<=10; i++)
+//     {document.write(
+//         userInput + "x" + i +"="+ 2*userInput, "<br>"
+//     )
+//     }
+// var arr = ["Milk" , "Bread" , "Apples"];
+// console.log(
+//     arr.splice(1,Banana)
+// )
+// var shopping = ["makeup","Bags","Jewllery","bag","purse",];
+// console.log(shopping);
+// console.log(shopping.length);
+// shopping[3]="purse"; //update modify
+// shopping[5]="eyeliner"; //add
+// shopping[6]="laptop";
+// shopping[-1]="lipstick";
+// console.log(shopping.length)
+// var shopping = ['makeup1','makeup2','makeup3','makeup4',
+// 'makeup5','makeup6', 'watch', 'bags', 'jewellery',
+// 'shoes', 'shoes','shoes','shoes','shoes','last'];
+// var lastElement = shopping[shopping.length - 1]
+// var someOtherElement = shopping[shopping.length - 2]
+// var someElement = shopping[1+1]; // makeup3
+// console.log(shopping[30]); //undefined as 30 index not found
+// console.log(shopping.length - 1);
+// console.log(shopping);
+// shopping[3] = 'purse'; //update/modify
+// shopping[5] = 'eyeliner'; // add
+// shopping[6] = 'laptop'
+// shopping[-1] = 'lipstick'
+// console.log(shopping);
+// var empty = [];
+// empty[0] = 1
+// console.log(empty.length); //0
+  
+
+// var tea = ["water", "milk","elaichi"];
+// console.log("🚀 ~ file: script.js:191 ~ tea:", tea)
+// tea.splice(1,1,"tea","sugar");
+//ctrl alt L for console
+
+
+
+// var count = [1,2,3,6,7,8];
+
+// count.splice(3,0,4,5);
+// count.push(9,10)
+// console.log("🚀 ~ file: script.js:198 ~ count:", count)
+
+// var city = prompt("Enter Your City")
+// var gender = prompt("Enter Your gender")
+// var appliedfor = prompt("applied for dubai: yes or no")
+// var city1 = ["Karachi","Faisalabd","Lahore"]
+// if((city.toLowerCase === city1[0] || city.toLowerCase === city1[1] ||  city.toLowerCase === city1[2] ) && gender === "male" && appliedfor === "yes"){
+// console.log("You are eligible for job")
+// }
+// else{
+//     console.log("You are not eligible for this job")
+// }
+// var  num= +prompt("Enter Your Number");
+// if(num < 0)
+// {document.write("Negative Number")
+// }
+// else{
+//     document.write("Positive Number")
+// }
+
+// var  num = +prompt("Enter Your Number");
+// if(num % 5 == 0 || num % 11 == 0 )
+// {document.write("DivisableNumber")
+// }
+// else{
+//     document.write("Not Divisable Number")
+// }
+// console.log("a" < "b")
+// console.log(1 >= "1")
+// console.log(0==="zero")
+// var city = prompt("Enter Your City");
+// var gender = prompt("Enter Your gender");
+// var appliedfor = prompt("applied for dubai: yes or no");
+// var city1 = ["karachi", "faisalabad", "lahore"]; // Convert array elements to lowercase
+
+// if (city1.includes(city.toLowerCase()) && gender === "male" && appliedfor === "yes") {
+//   console.log("You are eligible for the job");
+// } else {
+//   console.log("You are not eligible for this job");
+// }
+// if((true || false) && false)
+//git hyb me ctrl r click karengen to git hub ki previous command chal jae gi
+//  var fruits = ['banana','orange','watermelon','apple','mango','grapes','papaya'];
+//  var likedfruits = fruits.slice(2,7);
+//  console.log("🚀 ~ file: script.js:245 ~ likedfruits:", likedfruits)
+ // mdn web docs for interview preparation
+ 
+
+//  var userInput = +prompt("Enter your number");
+// //  var userInput2 = +prompt("Enter your number");
+//  for(var i=1; i<=10; i++)
+//     {document.write(
+//         userInput + "x" + i +"="+ userInput*i, "<br>"
+//     )
+//     }\
+//  var cities = ['dubai','maxico','chigago','LA','karachi'];
+//  var cityToCheck = prompt('Enter Your City');
+// for(let i = 0 ; i <= cities.length -1 ; i++){
+//     console.log('i=',i ,cities[i])
+    // if (cityToCheck === cities[i])
+    // {
+    //     alert('Your City is in the list');
+    // }
+// }
+
+// var i = ['1','2','3','4','5','6','7','8','9','10'];
+// for(var i=1; i<=10; i++)
+//     {console.log(i + 1
+//     )}
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// for(var i=0; i<=arr.length-1; i++)
+//     {console.log(arr[i]=arr[i]+1
+//     )}
+    // var userInput = +prompt("Enter your number");
+    // //  var userInput2 = +prompt("Enter your number");
+    //  for(var i=1; i<=10; i++)
+    //     {document.write(
+    //         userInput + "x" + i +"="+ userInput*i, "<br>"
+    //     )
+    //     }
+
+    
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+""
---
+++ variables.js
@@ -0,0 +1,28 @@
+// var myname ="Faiza";
+// var course ="WMA Batch-10";
+// var role ="Student";
+
+//  alert(myname +" "+ course + " " + role);
+//  concatination for space
+
+// var num1 = 20;
+// var num2 = 30;
+// var num3 = 20 + 30;
+// alert(num3);
+// var num3 = 30 - 20;
+// alert(num3);
+// var num1 = 20;
+// var num2 = 45;
+// var num3 = num1 / num2;
+// alert(num3);
+// 
+// var num1 = 20;
+// alert(num1 + '10');
+// var num1 = 20;
+// alert(num1 - '10');
+// var num1 = 20;
+// alert(num1 * '10');
+// var a;
+// a = 5;
+// var num = prompt("Enter your Number" , 10);
+// console.log("my num =", num + 2);
