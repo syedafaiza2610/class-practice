16c86b8201c05bd0fc74a31852adee44617bebc4
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-08 10:57:31 -0700

    functions

---
+++ function.js
@@ -0,0 +1,18 @@
+// regular,anonmyous,
+
+function myName(str) {
+    document.write(str)
+}
+var greet = "My name is Faiza";
+myName(greet);
+
+
+function sayHi(abc) {
+    console.log(abc);
+}
+var greet1 = "AOA";
+var greet2 = "Hi";
+var greet3 = "Hello";
+sayHi(greet1);
+sayHi(greet2);
+sayHi(greet3);
--- script.js
+++ script.js
@@ -427,15 +427,82 @@
     var add = sentence.indexOf(given);
     console.log("🚀 ~ file: script.js:428 ~ add:", add)
 
-    
 
+//     string methods, slice , splice, charAt, indexOf, lastIndexOf
+// var str = "i am in smit";
+// // ['a', 'b']
+// var firstChar = str.slice(0, 1) //i
+// console.log("str[2] = ", str[2]);
+// console.log("🚀 ~ file: strings.js:5 ~ firstChar:", firstChar)
+// var lastChar = str.slice(-1);
+// console.log("🚀 ~ file: strings.js:7 ~ lastChar:", lastChar)
+// var city = "karachi";
+// var firstChar = city.slice(0, 1);
+// var capitalFirst = firstChar.toUpperCase();
+// console.log("🚀 ~ file: strings.js:11 ~ firstChar:", firstChar)
+// var newCity = capitalFirst + city.slice(1)
+// console.log("🚀 ~ file: strings.js:14 ~ newCity:", newCity)
+// var name1 = "rafiyasultan" // Rafiya Sultan
+// var firstNameFirst = name1.slice(0, 1);
+// var space = 5;
+// var secNameFirst = name1.slice(6, 7);
+// var newName =  firstNameFirst.toUpperCase() + name1.slice(1, 6) + " "
+// + secNameFirst.toUpperCase() + name1.slice(7);
+// console.log("🚀 ~ file: strings.js:20 ~ newName:", newName)
+// var ins = "smit"
+// console.log(ins.toUpperCase())
+// console.log("faiza".toUpperCase());
+// //index charAt
+// console.log(ins.charAt(0)) // s
+// console.log(ins.charAt(4)) // undefined
+// console.log(ins.charAt(ins.length - 1)) //t
+// //using slice method 1
+// // to search n replace word or group of words
+// // var text = "iam happy. There is world war ii. hello world war ii"
+// // var search = "world war ii"
+// // var len = search.length //12
+// // for (let i = 0; i < text.length; i++) {
+// //     if(text.slice(i, i + len) === search){
+// //     text = text.slice(0, i) + "second world war" + text.slice(i+len)
+// //     }
+// // }
+// // console.log(text)
+
+
+// // using indexof
+// var text = "iam happy. 0 There is world war ii. hello world war ii"
+// var search = "world war ii"
+// var len = search.length //12
+// var searchIndex = text.indexOf(search) // address -1
+// console.log('asdsa',searchIndex,text.slice(searchIndex, searchIndex + len))
+// if(searchIndex !== -1){ // if found
+//     text = text.slice(0, searchIndex) + "second world war" + text.slice(searchIndex+len)
+//     }
+// console.log(text)
+
+// var searchIndex1 = text.indexOf('i', 2) // address 0
+// console.log("🚀 ~ file: strings.js:54 ~ searchIndex1:", searchIndex1)
+
+
+
+// document.write(Math.ceil(0.9)); //1
+// document.write(Math.ceil(3.03));//4
+// document.write(Math.ceil(1.89));//2
+// document.write(Math.ceil(2.0));//2
+  
     
       
+// document.write(Math.floor(0.9)); //0
+// document.write(Math.floor(3.03));//3
+// document.write(Math.floor(1.89));//1
+// document.write(Math.floor(2.0));//2
 
       
 
+// var randomNumb =(Math.ceil( Math.random()*6));
+// document.write(randomNumb);
 
-
+// var randomNumb =(Math.floor( Math.random()*6)+1);
 
 
 
