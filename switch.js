573ffef1005b634bb799350cdc24ebaac691c8ea
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-22 10:59:40 +0500

    while loop

--- switch.js
+++ switch.js
@@ -50,3 +50,158 @@
 //     document.write(i+" "+j)
 // }
 // }
+
+
+// @@ -51,3 +51,18 @@ for (let i = 0; i < nArr.length; i++) {
+// }
+// console.log("object " + "is our " + "" + "next" + " topic");
+// // object is our next topic
+
+// // what will be the result
+// for (let i = 1; i <= 2; i++) {
+// for (let j = 1; j <= 3; j++) {
+//     console.log(i*j)        // 1 2 3 2 4 6 
+// }
+// }
+
+// // how to update value of the fourth element
+// var numArr = [1, 7, 3];
+// //a numArr[4] = 6
+// //b numArr[3] = 6
+// //b numArr[0] = 6
+// // what is the operator in which both or either one is true then it gives true
+// @@ -21,4 +21,33 @@ if (firstname.length === lastname.length){
+//     fullname.push(firstname[i]+lastname[i])
+// }
+// }
+// console.log("🚀 ~ file: quiz.js:22 ~ fullname:", fullname)
+// console.log("🚀 ~ file: quiz.js:22 ~ fullname:", fullname)
+// // 
+// const arr = ["a", "b", "c"]
+// // for (let i = 0; i < arr.length; i++) {
+// //     document.write(arr[i])
+// // }
+// // const cleanestCities = ["fsl", "lhr", "mul"]
+// // var cityToCheck = "fsl"
+// // var len = cleanestCities.length;
+// // var matchFound = false;
+// // for (var i = 0; i < len; i++){
+// //     if (cityToCheck === cleanestCities[i]) {
+// //     matchFound = true;
+// //     alert("It's one of the cleanest cities");
+// //     break;
+// //     } 
+// // }
+// //  if (matchFound === false) {
+// //  alert("It's not on the list");
+// //  }
+
+// var nArr = [[1, 2, 3, 5], [6, 7], [8, 9]] // 2 dimension array
+// // console.log(nArr[1][1]); //4
+// for (let i = 0; i < nArr.length; i++) {
+// for (let j = 0; j < nArr[i].length; j++) {
+//     console.log(nArr[i][j]);       
+// }
+// }
+// console.log("object " + "is our " + "" + "next" + " topic");
+// // object is our next topic
+// 77 changes: 77 additions & 0 deletions77  
+// switch.js
+// @@ -0,0 +1,77 @@
+// // var price = 0;
+// // var menu = prompt("enter menu name");
+// // fish 1000, chowmein 400, biryani 300, pasta 500
+// // if(menu === "fish") {
+// //     price = 1000
+// // } else if (menu === "chowmein") {
+// //     price = 400
+
+// // }else if (menu === "biryani") {
+// //     price = 300
+// // } else if (menu === "pasta"){
+// //     price = 500
+// //     document.write("your bill is ", price)
+// //  } else {
+// //     price = 100
+
+// // }
+// // document.write("your bill is ", price)
+// // var price = 0;
+// // var menu = prompt("enter menu name");
+
+// /// switch
+// // switch (menu) {
+// //   case "lobster":
+// //   case "fish":
+// //     price = 1000;
+// //     break;
+// //   case "biryani" || "pulao":
+// //     price = 300;
+// //     break;
+// //   case "pasta":
+// //     price = 500;
+// //     break;
+// //   default:
+// //     price = 100;
+// // }
+// // document.write("your bill is ", price);
+// var a = 12;
+// switch (a > 10) { //true
+// case 13:
+//     console.log(">10");
+//     break;
+// case 11:
+//     console.log("11");
+//     break;
+// case true:
+//     console.log("true");
+//     break;
+// default:
+// console.log("default");
+// break;
+// }
+
+// const foo = 0;
+// switch (foo) { // 0
+// case -1:
+// console.log("negative 1");
+// break;
+// case 0: // Value of foo matches this criteria; execution starts from here
+// console.log(0);
+// break
+// case 1: // no break statement in 'case 0:' so this case will run as well
+// console.log(1);
+// break; // Break encountered; will not continue into 'case 2:'
+// case 2:
+// console.log(2);
+// break;
+// default:
+// console.log("default");
+// }
+// // Logs 0 and 1
+
+
+// // generate random # 0-5 and use switch to display the num
+// var rand = Math.round(Math.random() * 5 )
+// console.log("🚀 ~ file: switch.js:76 ~ rand:", rand)
+
+
+// switch (rand) {
+//     case 0:
+//          break;
+//          case 0:
+//             console.log("you entered one")
+//             break;
+//             case 1:
+//                 console.log("you entered one")
+//          break;
+//          case 2:
+//             console.log("you entered one")
+//          break;
+//          case 3:
+//             console.log("you entered one")
+//          break;
+//          default:
+//          break;
+// }
+
---
+++ while.js
@@ -0,0 +1,121 @@
+
+// var i = 0;
+// while (i<=15) {
+//     console.log("I am while")
+//     i++; 
+// }
+// var j = 0;
+// while (j< 10) {
+//     console.log(j)
+//     j++; 
+// }
+
+// let someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+// let notFound = true;
+// while (notFound && someArray.length > 0) {
+// if (someArray[0] === "Louiza") {
+// console.log("Found her!");
+//    notFound = false;
+//  } 
+// else {
+//    someArray.shift();
+//  }
+// }
+// var someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+// var notFound = true;
+// for (let i = 0 < someArray.length; i++; ) {
+//     if (someArray[0] === "Louiza") {
+//         console.log("Found her!");
+//            notFound = false;
+//          } 
+//          else{
+
+//          }  
+// }
+
+
+// var someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+// let i = 0;
+// while(i < someArray.length){
+//     console.log(someArray[i])
+//     if(someArray == "Louzia")
+// }
+
+// let i=1;
+// while (i<=10) {
+//     console.log("2" + "x" + i + "=" + 2*i)
+//     i++;
+// }
+// var pro1 = +prompt("Enter your number")
+// var num = 9;
+
+// if (pro1 === num) {
+//     alert("Its Your Lucky Number")
+// }
+// else{
+//     alert("Try Again")
+// }
+
+
+// 
+// method 1 
+// var num = 9;
+// let notFound = true
+// while (notFound) {
+//     var pro1 = +prompt("Enter your number")
+//      if(pro1 === num){
+//         alert("You Win")
+//         notFound = false
+//     }
+// }
+
+// method 2
+// const luckyNum = 3;
+// let guess =+prompt("Enter your number 1- 10");
+
+// while(guess !== luckyNum){
+//     guess =+prompt("Enter your number 1- 10");
+// }
+// if (guess === luckyNum) {
+//     document.write("You guess the number")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
